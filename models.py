08cd7b58f4a65630e8f535cff28a110a8310ae07
groscht <tobias.grosch@daimler.com>
2022-10-18 11:05:05 +0000

    Task 1 completed

--- models.py
+++ models.py
@@ -32,23 +32,19 @@ class NearEarthObject:
     initialized to an empty collection, but eventually populated in the
     `NEODatabase` constructor.
     """
-    # TODO: How can you, and should you, change the arguments to this constructor?
-    # If you make changes, be sure to update the comments in this file.
-    def __init__(self, designation, hazardous=False, name=None, diameter=float('nan'), **info):
+
+    def __init__(self, designation, name=None, diameter=float('nan'), hazardous=False):
         """Create a new `NearEarthObject`.
 
-        :param info: A dictionary of excess keyword arguments supplied to the constructor.
+        :param designation: A string containing the primary designation for this NearEarthObject.
+        :param name: A string containing the IAU name for this NearEarthObject.
+        :param diameter: A float for the diameter in kilometer of this NearEarthObject.
+        :param hazardous: A boolean weather ot nor this NearEarthObject is potentially hazardous.
         """
-        # TODO: Assign information from the arguments passed to the constructor
-        # onto attributes named `designation`, `name`, `diameter`, and `hazardous`.
-        # You should coerce these values to their appropriate data type and
-        # handle any edge cases, such as a empty name being represented by `None`
-        # and a missing diameter being represented by `float('nan')`.
         self.designation = str(designation)
-        self.name = str(name)
+        self.name = name
         self.diameter = float(diameter)
-        self.hazardous = hazardous
-        self.info = info
+        self.hazardous = True if hazardous == "Y" else False
 
         # Create an empty initial collection of linked approaches.
         self.approaches = []
@@ -56,15 +52,17 @@ class NearEarthObject:
     @property
     def fullname(self):
         """Return a representation of the full name of this NEO."""
-        # TODO: Use self.designation and self.name to build a fullname for this object.
-        return ''
+        if self.name is not None:
+            self._fullname = f"{self.designation} ({self.name})"
+        else:
+            self._fullname = self.designation
+        return self._fullname
 
     def __str__(self):
         """Return `str(self)`."""
-        # TODO: Use this object's attributes to return a human-readable string representation.
-        # The project instructions include one possibility. Peek at the __repr__
-        # method for examples of advanced string formatting.
-        return f"A NearEarthObject ..."
+        return f"The NearEarthObject '{self.fullname}' has {'an unknown diameter' if self.diameter == float('nan') else f'a diameter of {self.diameter:.3f}'}" \
+               f" km and {'is' if self.hazardous else 'is not'} potentially hazardous."
+
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
@@ -85,21 +83,18 @@ class CloseApproach:
     private attribute, but the referenced NEO is eventually replaced in the
     `NEODatabase` constructor.
     """
-    # TODO: How can you, and should you, change the arguments to this constructor?
-    # If you make changes, be sure to update the comments in this file.
-    def __init__(self, **info):
+    def __init__(self, designation, time, distance, velocity):
         """Create a new `CloseApproach`.
 
-        :param info: A dictionary of excess keyword arguments supplied to the constructor.
+        :param designation: A string containing the primary designation of the close approach's NEO.
+        :param time: A datetime object containing date and time, in UTC, at which the NEO passes closest to Earth.
+        :param distance: A float containing the nominal approach distance, in astronomical units, of the NEO to Earth at the closest point.
+        :param velocity: A float containing the velocity, in kilometers per second, of the NEO relative to Earth at the closest point.
         """
-        # TODO: Assign information from the arguments passed to the constructor
-        # onto attributes named `_designation`, `time`, `distance`, and `velocity`.
-        # You should coerce these values to their appropriate data type and handle any edge cases.
-        # The `cd_to_datetime` function will be useful.
-        self._designation = ''
-        self.time = None  # TODO: Use the cd_to_datetime function for this attribute.
-        self.distance = 0.0
-        self.velocity = 0.0
+        self._designation = str(designation)
+        self.time = cd_to_datetime(time)  # TODO: Use the cd_to_datetime function for this attribute.
+        self.distance = float(distance)
+        self.velocity = float(velocity)
 
         # Create an attribute for the referenced NEO, originally None.
         self.neo = None
@@ -117,17 +112,12 @@ class CloseApproach:
         formatted string that can be used in human-readable representations and
         in serialization to CSV and JSON files.
         """
-        # TODO: Use this object's `.time` attribute and the `datetime_to_str` function to
-        # build a formatted representation of the approach time.
-        # TODO: Use self.designation and self.name to build a fullname for this object.
-        return ''
+        return datetime_to_str(self.time)
 
     def __str__(self):
         """Return `str(self)`."""
-        # TODO: Use this object's attributes to return a human-readable string representation.
-        # The project instructions include one possibility. Peek at the __repr__
-        # method for examples of advanced string formatting.
-        return f"A CloseApproach ..."
+        return f"At {self.time_str}, '{self.neo.fullname if self.neo is not None else None}' approaches Earth at a distance of" \
+               f" {self.distance:.2f} au and a velocity of {self.velocity:.2f} km/s."
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
