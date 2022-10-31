d46dae14a705dd57be8426b8d5c94d02f8827c4d
groscht <tobias.grosch@daimler.com>
2022-10-31 12:35:07 +0000

    Task 5 completed and tested

--- write.py
+++ write.py
@@ -12,6 +12,7 @@ You'll edit this file in Part 4.
 """
 import csv
 import json
+from helpers import datetime_to_str
 
 
 def write_to_csv(results, filename):
@@ -28,7 +29,19 @@ def write_to_csv(results, filename):
         'datetime_utc', 'distance_au', 'velocity_km_s',
         'designation', 'name', 'diameter_km', 'potentially_hazardous'
     )
-    # TODO: Write the results to a CSV file, following the specification in the instructions.
+    with open(filename, "w") as outfile:
+        writer = csv.writer(outfile)
+        writer.writerow(list(fieldnames))
+        for approach in results:
+            row = [approach.time, 
+                approach.distance, 
+                approach.velocity, 
+                approach.neo.designation, 
+                "" if approach.neo.name == "None" else approach.neo.name, 
+                approach.neo.diameter, 
+                approach.neo.hazardous
+            ]
+            writer.writerow(row)
 
 
 def write_to_json(results, filename):
@@ -42,4 +55,18 @@ def write_to_json(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    # TODO: Write the results to a JSON file, following the specification in the instructions.
+    with open(filename, "w") as outfile:
+        json_list = []
+        for approach in results:
+            approach_dict = {}
+            approach_dict["datetime_utc"] = datetime_to_str(approach.time)
+            approach_dict["distance_au"] = approach.distance
+            approach_dict["velocity_km_s"] = approach.velocity
+            approach_dict["neo"] = {"designation": approach.neo.designation,
+                                "name": approach.neo.name,
+                                "diameter_km": approach.neo.diameter,
+                                "potentially_hazardous": approach.neo.hazardous
+            }
+            json_list.append(approach_dict)
+
+        json.dump(json_list, outfile, indent = 2)
